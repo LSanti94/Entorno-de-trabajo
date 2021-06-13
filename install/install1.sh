cc08e3aaf4b75544b385da68c832a88893f077c9
Luis Santillán <64935656+LSanti94@users.noreply.github.com>
2021-06-13 05:37:44 +0000

    Update install1.sh

--- install/install1.sh
+++ install/install1.sh
@@ -8,9 +8,9 @@ sleep 3
 echo ""
 echo ""
 
-apt update && apt upgrade -y && apt install -y htop && apt install -y wget curl git && apt install -y zip unzip && apt install -y i3 i3status i3lock dmenu feh compton && apt install -y vim vim-gui-common vim-runtime && apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings && apt install -y terminator && apt install -y firefox-esr && apt install -y scrub && apt install -y cmatrix
+apt update && apt upgrade -y && apt install -y htop && apt install -y wget curl git && apt install -y zip unzip && apt install -y i3 i3status i3lock dmenu feh compton && apt install -y vim vim-gui-common vim-runtime && apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings && apt install -y terminator && apt install -y firefox-esr && apt install -y scrub && apt install -y cmatrix network-manager pulseaudio bluez && pulseaudio --start
 
-mkdir ~/Descargass && cd ~/Descargass && wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-musl_0.20.1_amd64.deb && export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin && dpkg -i *.deb && rm -fr *.deb
+mkdir ~/Descargas && cd ~/Descargass && wget https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-musl_0.20.1_amd64.deb && export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin && dpkg -i *.deb && rm -fr *.deb
 
 cd ~/Descargass ; wget https://github.com/sharkdp/bat/releases/download/v0.18.0/bat-musl_0.18.0_amd64.deb ; export PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin && dpkg -i *.deb ; rm -fr *.deb
 
@@ -20,6 +20,6 @@ cd ~/Descargass ; wget https://github.com/sharkdp/bat/releases/download/v0.18.0/
 
 apt install -y zsh-syntax-highlighting zsh-autosuggestions && mkdir /usr/share/zsh-sudo && cd /usr/share/zsh-sudo && wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh ; chmod +x sudo.plugin.zsh && cd  /usr/share/ && chown ls:ls -R zsh-*
 
-cd /root/ && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+cd /root/ && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
 apt install -y zsh
