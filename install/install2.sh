4850a5571daa6fc54038f1523aab5cc8963f3354
Luis Santillan <LSanti94>
2023-12-27 18:34:04 -0500

    update

--- install/install2.sh
+++ install/install2.sh
@@ -15,12 +15,16 @@ RO="\033[1;31m"  ## Color Rojo
 VE="\033[1;32m"  ## Color Verde
 CL="\e[0m"       ## Limpiar colores
 
-## Instalación fzf
-echo -e "$AM 1) Instalación fzf$VE"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+# Instalación ohmyzsh
+echo -e "$AM 2) Tema powerlevel10k$VE"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && chsh -s $(which zsh) || true
 
 ## Tema powerlevel10k
 echo -e "$AM 2) Tema powerlevel10k$VE"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+## Instalación fzf
+echo -e "$AM 1) Instalación fzf$VE"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
 echo -e "$VE Proceso completado$CL"
