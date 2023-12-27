3ed3533f9717ec9cf5a006e889e7bb4de73cced3
Luis Santillan <LSanti94>
2023-12-27 17:24:03 -0500

    update

--- install/install3.sh
+++ install/install3.sh
@@ -19,10 +19,6 @@ CL="\e[0m"       ## Limpiar colores
 echo -e "$AM 1) Instalación powerlevel10k$VE"
 sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/root/.oh-my-zsh/custom}/themes/powerlevel10k
 
-## Borrar .zshrc de root
-echo -e "$AM 2) Borrar .zshrc de root$VE"
-sudo rm -f /root/.zshrc
-
 ## Enlace simbólico para root
 echo -e "$AM 3) Enlace simbólico para root$VE"
 sudo ln -s -f /home/cloud-devops/.zshrc /root/.zshrc && sudo chsh -s $(which zsh)
