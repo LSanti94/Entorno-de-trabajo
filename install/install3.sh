95d4713c8d94b662be6ad8b4b8559ae2d3810b39
Luis Santillan <LSanti94>
2023-12-27 19:03:27 -0500

    update

--- install/install3.sh
+++ install/install3.sh
@@ -15,6 +15,10 @@ RO="\033[1;31m"  ## Color Rojo
 VE="\033[1;32m"  ## Color Verde
 CL="\e[0m"       ## Limpiar colores
 
+# Instalación ohmyzsh
+echo -e "$AM 2) Tema powerlevel10k$VE"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && chsh -s $(which zsh) || true
+
 ## Instalación powerlevel10k
 echo -e "$AM 1) Instalación powerlevel10k$VE"
 sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-/root/.oh-my-zsh/custom}/themes/powerlevel10k
